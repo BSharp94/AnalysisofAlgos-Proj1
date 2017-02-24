197f2e853809de226f0b3a7e9881bbfc3b437df7
Brian Sharp <briansharp1994@gmail.com>
2017-02-24 12:48:34 -0500

    Added timing script to main

--- GCD.py
+++ GCD.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-
+import datetime as dt
 
 def EuclidGCD(var1,var2):
 
@@ -9,7 +9,7 @@ def EuclidGCD(var1,var2):
         return var2
     if var2 == 0:
         return var1
-   
+
    #switch values so var1 is graeter
     if var2 > var1:
         temp = var1
@@ -43,15 +43,69 @@ def compare_divisors(var1,var2):
 
     return best_results
 
-
+'''
 var1, var2 = raw_input("Please enter two integers: ").strip().split()
 var1, var2 = int(var1),int(var2)
 
 #run Euclidean algorithm
 print "==== Euclid Method ===="
-print "Results: ",(EuclidGCD(var1,var2))
+n1 = dt.datetime.now()
+results = (EuclidGCD(var1,var2))
+n2 = dt.datetime.now()
+print "Results: ", results
+print "Time: ", (n2-n1).microseconds
 
 #run comparing divisors
 print "=== Compare Divisors ==="
-print "Results: ",(compare_divisors(var1,var2))
+n1 = dt.datetime.now()
+results = (compare_divisors(var1,var2))
+n2 = dt.datetime.now()
+print "Results: ",results
+print "Time: ", (n2-n1).microseconds
+'''
+def get_average_time_euclid(min_val,max_val,sample_size):
+    #record time taken against different sizes of inputs
+    data_0_to_100_val1 = [ np.random.randint(min_val,max_val) for i in range(sample_size)]
+    data_0_to_100_val2 = [ np.random.randint(min_val,max_val) for i in range(sample_size)]
 
+    global_sum = 0
+    for index in range(0,sample_size):
+        n1 = dt.datetime.now()
+        results = (EuclidGCD(data_0_to_100_val1[index],data_0_to_100_val2[index]))
+        n2 = dt.datetime.now()
+        global_sum += (n2-n1).microseconds
+    return (global_sum/sample_size)
+
+
+def get_average_time_compare_divisors(min_val,max_val,sample_size):
+    #record time taken against different sizes of inputs
+    data_0_to_100_val2 = [ np.random.randint(min_val,max_val) for i in range(sample_size)]
+    data_0_to_100_val1 = [ np.random.randint(min_val,max_val) for i in range(sample_size)]
+
+    global_sum = 0
+    for index in range(0,sample_size):
+        n1 = dt.datetime.now()
+        results = (compare_divisors(data_0_to_100_val1[index],data_0_to_100_val2[index]))
+        n2 = dt.datetime.now()
+        global_sum += (n2-n1).microseconds
+    return (global_sum/sample_size)
+
+
+if __name__ == "__main__":
+    sample_size = 100
+
+    values = [10e1,10e2,10e3,10e4,10e5,10e6,10e7]
+
+    print "==== Average Time for Euclids Algorithm ====="
+
+    for index in range(len(values)-1):
+        val1,val2 = values[index],values[index+1] #get range values
+        avg_time = get_average_time_euclid(val1,val2,sample_size)
+        print "Range {} to {}: avg_time {}".format(val1,val2,avg_time)
+
+    print "==== Average Time for Compare Divisors Algorithm ====="
+
+    for index in range(len(values)-1):
+        val1,val2 = values[index],values[index+1] #get range values
+        avg_time = get_average_time_compare_divisors(val1,val2,sample_size)
+        print "Range {} to {}: avg_time {}".format(val1,val2,avg_time)
